e1948460a5c9b739af91e1d6453ed3e31af17152
Nathaniel Nicholas <nn222qn@student.lnu.se>
2023-11-06 20:51:02 +0100

    Testing

---
+++ Main.py
@@ -0,0 +1,10 @@
+import random as rd
+
+digit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+payment_network = ['VISA', 'MasterCard', 'AMEX', 'Discover']
+credit_card = ''
+for d in range(16):
+    credit_card += str(rd.choice(digit))
+blurred_card = f"{'X' * 4} {'X' * 4} {'X' * 4} {credit_card[12:16]}"
+print(f"{blurred_card}\t{rd.choice(payment_network)}")
+# TESTING TESTING
