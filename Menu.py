7d1284a5fd74ffa982fb019e7a5873aacd5196cf
Nathaniel Nicholas <nn222qn@student.lnu.se>
2023-11-06 23:16:11 +0100

    More menu options.

--- Menu.py
+++ Menu.py
@@ -10,57 +10,70 @@ class Menu:
         ('Mocha', 6.45)
     ]
     teas = [
-        ('Black tea', 3.75),
+        ('Black Tea', 3.75),
         ('Earl Grey', 4.00),
         ('Peppermint', 4.25),
         ('Chai Tea', 5.00)
     ]
-    cold_beverages = [
-        'Cold Brew', 'Iced Americano', 'Iced Coffee',
-        'Iced Shaken Espresso', 'Iced Flat White', 'Iced Latte',
-        'Iced Macchiato', 'Iced Mocha'
+    cold_coffees = [
+        ('Cold Brew', 4.40),
+        ('Iced Americano', 4.05),
+        ('Iced Coffee', 3.85),
+        ('Iced Shaken Espresso', 6.30),
+        ('Iced Flat White', 4.00),
+        ('Iced Latte', 6.30),
+        ('Iced Macchiato', 6.50),
+        ('Iced Mocha', 6.65)
     ]
     seasonal_beverages = [
         'Christmas Drink or something'
     ]
+    shopping_cart = []
 
-    def hot_drinks():
-        options = Menu.hot_coffees
-        options += Menu.teas
-        print('Our hot beverages are:')
+    def select_items(prompt, options):
         for i, (name, price) in enumerate(options):
-            print(f'{i + 1}\t{name}\t{price}')
+            print(f'{i + 1}\t{name:<20} {price:.2f}')
+        print('Q for quit. ')
+        choice = input(f'Which {prompt} would you like? ')
         try:
-            choice = int(input('Which drink would you like? '))
-            choice = choice - 1
-            print(choice)
+            choice = int(choice) - 1
+            Menu.shopping_cart.append(options[choice])
+            print(Menu.shopping_cart)
         except ValueError:
+            if 'q' in choice.lower():
+                return False
             print('It has to be a number')
-
-    def cold_drinks():
-        pass
-
-    def seasonal_drinks():
-        pass
 
     def payments():
         pass
 
     def menu():
-        answer = input('1) Drinks\n2) Food\n3) Merchandise\n4) Quit\nChoice: ')
-        if answer.isnumeric():
-            answer = int(answer)
+        try:
+            answer = int(input('1) Drinks\n2) Food\n3) Merchandise\n4) Quit\nChoice: '))
             if answer == 1:
-                Menu.hot_drinks()
+                try:
+                    answer = int(input('1) Hot Beverages\n2) Cold Beverages\nChoice: '))
+                    if answer == 1:
+                        print('Our hot beverages are:')
+                        if Menu.select_items('hot beverage', Menu.hot_coffees + Menu.teas) is False:
+                            return False
+                    elif answer == 2:
+                        print('Our cold beverages are:')
+                        Menu.select_items('cold beverage', Menu.cold_coffees)
+                    else:
+                        print('You must choose between 1 and 2')
+                except ValueError:
+                    print('You must enter a number!')
             elif answer == 2:
-                Menu.cold_drinks()
+                print('Our food items are:')
+                Menu.select_items('food item', Menu.cold_coffees)
             elif answer == 3:
                 Menu.seasonal_drinks()
             elif answer == 4:
                 Menu.payments()
             else:
                 print('You must enter a value between 1 and 4!')
-                Menu.menu()
-        else:
+        except ValueError:
             print('You must enter a number!')
-            Menu.menu()
+
+        Menu.menu()
