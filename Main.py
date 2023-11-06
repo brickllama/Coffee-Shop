cb8f9186616034e25f1d981aaf5d664cdf605130
Nathaniel Nicholas <nn222qn@student.lnu.se>
2023-11-06 22:35:35 +0100

    Calls the menu.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import random as rd
+from Menu import Menu
 
 digit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 payment_network = ['VISA', 'MasterCard', 'AMEX', 'Discover']
@@ -7,4 +8,5 @@ for d in range(16):
     credit_card += str(rd.choice(digit))
 blurred_card = f"{'X' * 4} {'X' * 4} {'X' * 4} {credit_card[12:16]}"
 print(f"{blurred_card}\t{rd.choice(payment_network)}")
-# this now works
+
+Menu.menu()
